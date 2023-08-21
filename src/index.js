c2ee7d391b9523a83380e6631c32e0cb53bf8774
Pham Thanh Vu <vu.pham@terralogicvietnam.com>
2023-08-21 10:58:11 +0700

    test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello Vu!');
+    res.send('Hello There! Im a bad deployment process service');
 });
 
 app.listen(port, () => {
